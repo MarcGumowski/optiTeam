4860a4ba6f5564703ad09d2a26b7bf7fe8556b51
MarcGumowski <marc.gumowski@gmail.com>
2017-10-22 01:38:30 +0200

    Created js files

---
+++ js/optiTeam.js
@@ -0,0 +1,10 @@
+// 1. Run scrap data script
+
+// 2. parseFloat(document.getElementById("budget").value
+
+// 3. run optiTeam function
+
+// optiTeam function
+// -> data
+// -> algo
+// -> store results in different id table
