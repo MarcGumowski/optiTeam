129b3099e7eef9809f9b915209bd6105c7e2f4cc
Marc Gumowski <marc.gumowski@gmail.com>
2018-01-21 18:15:19 +0100

    added d3, started to work on svg dimensions

--- js/playerPerfChart.js
+++ js/playerPerfChart.js
@@ -9,3 +9,25 @@
 //               
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+var margin = { top: 20, left: 20, right: 20, bottom: 20 };
+var width = 960 - margin.left - margin.right;
+var height = 560 - margin.top - margin.bottom;
+
+var div = d3.select('#playerChart').append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
+
+var svg = d3.select("#playerChart").append("svg")
+    .attr('id', 'playerChartSvg')
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right);
+    
+var g = svg.append("g")
+    .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
+    
+var formatNumber = d3.format(".2f"),
+    format = function(d) { return formatNumber(d); };  
+    
+    
+    
+    
